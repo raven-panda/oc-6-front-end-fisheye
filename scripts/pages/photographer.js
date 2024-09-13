2e2c4ae8bd5bbe600d3445354777baeb13fb95f0
Ambroise Maire <a.maire@kiwilab.fr3>
2024-09-13 15:51:24 +0200

    add: implemented photographer header in photographer page

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,35 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+import { PhotographerService } from "../services/photographerService.js";
+import { photographerTemplate } from "../templates/photographer.js";
+
+async function photographerPage() {
+  const params = new URLSearchParams(document.location.search);
+  const photographerService = PhotographerService();
+  const photographerId = params.get("photographerId");
+
+  console.log(photographerId);
+
+  function displayPhotographerHeaderData(photographer) {
+    const photographerHeader = document.querySelector(".photograph-header");
+    const photoDescriptionBox = photographerHeader.querySelector(".photograph_description");
+    const photographerModel = photographerTemplate(photographer);
+
+
+    const photographerDescription = photographerModel.getUserCardDescriptionDOM();
+    photographerDescription.displayData(photoDescriptionBox, photoDescriptionBox, photoDescriptionBox);
+
+    const photographerPicture = photographerModel.getUserCardPictureDOM();
+    photographerPicture.displayData(photographerHeader);
+
+  }
+
+  function displayData(photographer) {
+    displayPhotographerHeaderData(photographer);
+  }
+
+  const photographer = await photographerService.getPhotographerById(photographerId);
+  displayData(photographer);
+}
+
+photographerPage();
